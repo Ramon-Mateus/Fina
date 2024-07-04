946281ae0234c9fc07cc665800e1f8d0173ebd16
Ramon de Oliveira Araújo Mateus <ramonmateus00@gmail.com>
2024-07-04 02:32:08 -0300

    feat: handle files (api) create category method

--- Fina.Api/Program.cs
+++ Fina.Api/Program.cs
@@ -1,4 +1,19 @@
+using Fina.Api.Data;
+using Fina.Api.Handlers;
+using Fina.Core.Handlers;
+using Microsoft.EntityFrameworkCore;
+
 var builder = WebApplication.CreateBuilder(args);
+
+const string ConnectionString = "Server=localhost,1433;Database=Fina;User ID=sa;Password=SenhaForte123#;Trusted_Connection=False;TrustServerCertificate=True;";
+
+builder.Services.AddDbContext<AppDbContext>(
+    options => options.UseSqlServer(ConnectionString)
+);
+
+builder.Services.AddTransient<ICategoryHandler, CategoryHandler>();
+builder.Services.AddTransient<ITransactionHandler, TransactionHandler>();
+
 var app = builder.Build();
 
 app.MapGet("/", () => "Hello World!");
